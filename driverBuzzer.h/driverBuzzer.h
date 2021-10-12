428e22e8929f81a8620d5c7503e669647f2ae5b1
andre-won <62629943+andre-won@users.noreply.github.com>
2021-10-12 15:52:06 -0300

    Add files via upload

---
+++ driverBuzzer.h/driverBuzzer.h
@@ -0,0 +1,42 @@
+
+/* ***************************************************************** */
+/* File name:        driverBuzzer.h                                  */
+/* File description: Header file containing the functions/methods for*/
+/*                   initializing and using the buzzer               */
+/* Author name:      André Won, Cassio Dezzoti, Totmés Scheffer,     */
+/*                   Guilherme Abreu                                 */
+/* Creation date:    11out2021                                       */
+/* Revision date:    11out2021                                       */
+/* ***************************************************************** */
+
+#ifndef DRIVERDISTANCIA_H
+#define DRIVERDISTANCIA_H
+
+/* ***************************************************************** */
+/* Method name:        initBuzzer                                    */
+/* Method description: Header file containing the functions/methods f*/
+/*                   initializing and using the ultrassound sensor   */
+/*Input params:                                                      */
+/*Output params:                                                     */
+/* ***************************************************************** */
+void initBuzzer(int var1);
+
+/* ***************************************************************** */
+/* Method name:        tocarAlarme                                   */
+/* Method description: Header file containing the functions/methods f*/
+/*                   initializing and using the ultrassound sensor   */
+/*Input params:                                                      */
+/*Output params:                                                     */
+/* ***************************************************************** */
+void tocarAlarme(void);
+
+/* ***************************************************************** */
+/* Method name:        pararAlarme                                   */
+/* Method description: Header file containing the functions/methods f*/
+/*                   initializing and using the ultrassound sensor   */
+/*Input params:                                                      */
+/*Output params:                                                     */
+/* ***************************************************************** */
+void pararAlarme(void);
+
+#endif /* DRIVERDISTANCIA_H */
