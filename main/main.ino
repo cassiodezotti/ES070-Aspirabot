a3dd9b84b0a0c2e3e018cd7eed28eff45e55a87b
Cássio Dezotti <cassiodezotti@gmail.com>
2021-11-11 17:04:41 -0300

    Feito teste com buzzer, utilizei um botao na porta 13 para controlar e o buzzer na porta 1. Mudei um pouco o som para parecer um alarme

--- main/main.ino
+++ main/main.ino
@@ -9,14 +9,14 @@
 
 
 //Portas só para ilustrar, o planejamento das portas será feito depois
-byte pinosLcd[6] = {1,2,3,4,5,6};
-SensorDistancia sensorEsquerda(1,1);
-SensorDistancia sensorFrontal(1,1);
-SensorDistancia sensorDireito(1,1);
-DriverLocomocao motoresLocomocao(1,2,3,4);
-DriverEscovas motoresEscovas(1,2,3,4);
+//byte pinosLcd[6] = {1,2,3,4,5,6};
+//SensorDistancia sensorEsquerda(1,1);
+//SensorDistancia sensorFrontal(1,1);
+//SensorDistancia sensorDireito(1,1);
+//DriverLocomocao motoresLocomocao(1,2,3,4);
+//DriverEscovas motoresEscovas(1,2,3,4);
 DriverBuzzer buzzer(1);
-DriverLcd lcd(pinosLcd);
+//DriverLcd lcd(pinosLcd);
 //Bateria bateriaSistema(1,2);
 int objetosDetectados = 0;
 float distanciaMin = 0.0;
@@ -26,12 +26,17 @@ int modoOperacao; //Depois será testado com enum
 
 void setup() {
   // put your setup code here, to run once:
-
+  pinMode(13,INPUT_PULLUP);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  if(digitalRead(13) == HIGH){
+    buzzer.tocarAlarme();
+  }else{
+    buzzer.pararAlarme();
+  }
+  
 }
 
 void iniciarPlaca() {
