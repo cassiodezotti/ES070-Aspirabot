ae58f4b14c2f70426295563477a06bcc073110d6
andre-won <62629943+andre-won@users.noreply.github.com>
2021-11-27 14:07:31 -0300

    Update main.ino
    adding 3 ultrasound sensors

--- main/main.ino
+++ main/main.ino
@@ -10,9 +10,9 @@
 
 //Portas só para ilustrar, o planejamento das portas será feito depois
 //byte pinosLcd[6] = {1,2,3,4,5,6};
-SensorDistancia sensorEsquerda(11,12);
-//SensorDistancia sensorFrontal(1,1);
-//SensorDistancia sensorDireito(1,1);
+SensorDistancia sensorDireita(2,3);
+SensorDistancia sensorEsquerda(5,6);
+SensorDistancia sensorFrente(8,9);
 //DriverLocomocao motoresLocomocao(1,2,3,4);
 //DriverEscovas motoresEscovas(1,2,3,4);
 DriverBuzzer buzzer(1);
@@ -27,8 +27,15 @@ int modoOperacao; //Depois será testado com enum
 void setup() {
   // put your setup code here, to run once:
   pinMode(13,INPUT_PULLUP);
-  pinMode(11,INPUT);
-  pinMode(12,OUTPUT);
+  
+  pinMode(2,INPUT);
+  pinMode(3,OUTPUT);
+
+  pinMode(5,INPUT);
+  pinMode(6,OUTPUT);
+
+  pinMode(8,INPUT);
+  pinMode(9,OUTPUT);  
 }
 
 void loop() {
@@ -39,10 +46,24 @@ void loop() {
     buzzer.pararAlarme();
   }
   
-  float distancia;
-  distancia = sensorEsquerda.getDistance();
-  Serial.println(distancia);
-  delay(500).
+  float distancia1;
+  float distancia2;
+  float distancia3;
+  
+  distancia1 = sensorDireita.getDistance();
+  Serial.print("Sensor Direita: ");
+  Serial.println(distancia1);
+  delay(300);
+
+  distancia2 = sensorEsquerda.getDistance();
+  Serial.print("Sensor Esquerda: ");
+  Serial.println(distancia2);
+  delay(300);
+  
+  distancia3 = sensorFrente.getDistance();
+  Serial.print("Sensor Frente: ");
+  Serial.println(distancia3);
+  delay(300);
   
 }
 
